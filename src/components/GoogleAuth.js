e642c1b312bd374d0548594a1c0e78a1ad736466
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2021-06-11 21:50:48 -0300

    feat/oauth setup

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -23,22 +23,32 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() })
   };
 
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if ( this.state.isSignedIn === null ) {
       return(
-        <div>
-          I don't know if we are signed in
-        </div>
+        null
       )
     } else if (this.state.isSignedIn) {
       return(
-        <div>I am signed in</div>
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon"/>
+          Sign Out
+        </button>
       )
     } else {
       return (
-        <div>
-          I am not singed in 
-        </div>
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon"/>
+          Sign In with Google
+        </button>
       );
     }
   };
