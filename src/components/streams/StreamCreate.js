b99792884b3b5b974c0041f372eb8e7caa0da291
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2021-07-14 21:38:18 -0300

    feat:some rest added

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, formValues, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../ducks/actions'
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -23,8 +25,8 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit (formValues) {
-    console.log(formValues);
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
   };
 
   render () {
@@ -64,7 +66,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm ({
   form: 'streamCreate',
   validate: validate
 })(StreamCreate);
+
+export default connect( null, {createStream} )(formWrapped);
