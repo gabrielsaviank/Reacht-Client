4da2116b2084b4eeae3ec84faf93b117e265b39f
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2021-07-21 09:21:20 -0300

    feat: rest reducers added

---
+++ src/ducks/reducers/streamReducer.ts
@@ -0,0 +1,19 @@
+import {
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    CREATE_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions/types';
+
+export default ( state = {}, action: any ) => {
+    switch (action.type) {
+        case FETCH_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case CREATE_STREAM: 
+            return { ...state, [action.payload.id]: action.payload };
+        case EDIT_STREAM: 
+            return {... state, [action.payload.id]: action.payload };
+        default: state
+    }
+};
