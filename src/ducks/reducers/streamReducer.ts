53641fd05ae276480bd69e4d71e66a711212abd9
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2021-07-22 09:14:46 -0300

    feat: reducers implemented

--- src/ducks/reducers/streamReducer.ts
+++ src/ducks/reducers/streamReducer.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+import _ from 'lodash';
 import {
     FETCH_STREAM,
     FETCH_STREAMS,
@@ -6,14 +8,19 @@ import {
     DELETE_STREAM
 } from '../actions/types';
 
-export default ( state = {}, action: any ) => {
+
+export default ( state: {}, action: any ) => {
     switch (action.type) {
+        case FETCH_STREAMS: 
+            return { ...state, ..._.mapKeys(action.payload, 'id' ) };
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case CREATE_STREAM: 
             return { ...state, [action.payload.id]: action.payload };
         case EDIT_STREAM: 
             return {... state, [action.payload.id]: action.payload };
+        case DELETE_STREAM: 
+            return _.omit(state, action.payload);
         default: state
     }
 };
