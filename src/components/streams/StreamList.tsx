6472b7bb26d95f75f33490bd291f913518744743
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2021-07-26 21:05:25 -0300

    feat: rendering streams

--- src/components/streams/StreamList.tsx
+++ src/components/streams/StreamList.tsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { fetchStreams } from '../../ducks/actions';
 
 interface StreamProps {
+  streams?: object | any,
   fetchStreams?: any,
 };
 
@@ -10,9 +11,38 @@ class StreamList extends React.Component<StreamProps> {
   componentDidMount () {
     this.props.fetchStreams();
   };
+
+  renderList = () => {
+    return this.props.streams.map((stream: any) =>  {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera"/>
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  };
+
   render(){
-    return <div>StreamList</div>
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">
+          {this.renderList()}
+        </div>
+      </div>
+    )
   }
 };
 
-export default connect(null, {fetchStreams})(StreamList);
+const mapStateToProps = ( state: any) => {
+  return { streams: Object.values(state.streams)};
+};
+
+export default connect( 
+  mapStateToProps,
+  {fetchStreams}
+)(StreamList);
