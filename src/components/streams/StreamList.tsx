36b4ab6b53545aa5414acd2d18ebdf38cf4ed92c
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2021-07-27 09:14:18 -0300

    feat: wired posts to users

--- src/components/streams/StreamList.tsx
+++ src/components/streams/StreamList.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../ducks/actions';
+import { Link } from 'react-router-dom';
 
 interface StreamProps {
   streams?: object | any,
   fetchStreams?: any,
+  currentUserId?: string,
+  isSignedIn?: boolean,
 };
 
 class StreamList extends React.Component<StreamProps> {
@@ -12,10 +15,27 @@ class StreamList extends React.Component<StreamProps> {
     this.props.fetchStreams();
   };
 
+  renderAdmin(stream: any) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">
+            Edit
+          </button>
+
+          <button className="ui button negative">
+            Delete
+          </button>
+        </div>
+      );
+    };
+  };
+
   renderList = () => {
     return this.props.streams.map((stream: any) =>  {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera"/>
           <div className="content">
             {stream.title}
@@ -26,12 +46,25 @@ class StreamList extends React.Component<StreamProps> {
     });
   };
 
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    };
+  };
+
   render(){
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">
           {this.renderList()}
+          {this.renderCreate()}
         </div>
       </div>
     )
@@ -39,7 +72,11 @@ class StreamList extends React.Component<StreamProps> {
 };
 
 const mapStateToProps = ( state: any) => {
-  return { streams: Object.values(state.streams)};
+  return { 
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
+  };
 };
 
 export default connect( 
