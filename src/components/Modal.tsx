5ef4211623eb06b4750dcea8ba728cbf4d9a005a
gabrielsavian <gabrielsvnzrd@gmail.com>
2021-10-15 19:51:17 -0300

    feat: new props

---
+++ src/components/Modal.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+interface DeleteProps {
+    title: string,
+    content: string
+    actions?: any
+    onDismiss?: () => void
+};
+
+const Modal = ( props: DeleteProps ) => {
+    return ReactDOM.createPortal(
+        <div
+            onClick={props.onDismiss}
+            className="ui dimmer modals visible active"
+        >
+            <div
+                onClick={e => e.stopPropagation()}
+                className="ui standard modal visible active"
+            >
+                <div className="header">{props.title}</div>
+                <div className="content">
+                    {props.content}
+                </div>
+                <div className="actions">
+                    {props.actions}
+                </div>
+            </div>
+        </div>,
+        document.querySelector('#modal')!
+    );
+};
+
+export default Modal;
